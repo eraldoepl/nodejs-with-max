60f4748a66f0019d2c457e5ed6f578094d87e981
eraldoepl <eraldoepl@gmail.com>
2022-10-14 10:48:24 -0400

    new features

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
-exports.notFoundPage = (req, res, next) => {
-  res.status(404).render("not-found", { pageTitle: "No Found" });
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const productFile = path.join(__dirname, "..", "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(productFile, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(productFile, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
